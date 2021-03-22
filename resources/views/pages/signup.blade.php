6aecfb4ddc550017b321838890c2870b32ba99cd
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-03-23 00:05:05 +0600

    Added all required files and created complete login page

---
+++ resources/views/pages/signup.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Signup</title>
+    <link rel="stylesheet" href="assets/css/signup.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="page-title"></div>
+      <div class="heading"></div>
+      <div class="signup-form">
+        <form method="post">
+          <div class="agree"></div>
+          <div class="buttons"></div>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
