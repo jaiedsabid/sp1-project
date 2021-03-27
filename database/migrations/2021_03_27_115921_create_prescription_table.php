c07cfe6ee8ecfde96eb4883543f0bda2f359ff0b
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-03-27 18:09:58 +0600

    Created migration files and models

---
+++ database/migrations/2021_03_27_115921_create_prescription_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrescriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prescription', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')
+                ->constrained('patients')
+                ->onDelete('cascade');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->string('prescription');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prescription');
+    }
+}
