9600d03ad917e015906b0426b612db54952fccd1
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-03-27 14:35:45 +0600

    Added patient profile and list pages and assets

---
+++ resources/views/pages/patient-list.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Patients list</title>
+    <link rel="stylesheet" href="assets/css/patient-list.css" />
+  </head>
+  <body>
+    <div id="container">
+      <div id="page-title">
+        <h2>Patients list</h2>
+      </div>
+      <div id="back-btn">
+        <a href="#">Back</a>
+      </div>
+      <div id="list">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Mobile</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a href="#">1</a></td>
+              <td><a href="#">A</a></td>
+              <td><a href="#">012345678911</a></td>
+            </tr>
+            <tr>
+              <td><a href="#">1</a></td>
+              <td><a href="#">A</a></td>
+              <td><a href="#">012345678911</a></td>
+            </tr>
+            <tr>
+              <td><a href="#">1</a></td>
+              <td><a href="#">A</a></td>
+              <td><a href="#">012345678911</a></td>
+            </tr>
+            <tr>
+              <td><a href="#">1</a></td>
+              <td><a href="#">A</a></td>
+              <td><a href="#">012345678911</a></td>
+            </tr>
+          </tbody>
+        </table>
+        <div id="paginate" class="flex flex-center flex-space-evenly">
+          <a href="#"><< Prev</a>
+          <a href="#">Next >></a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
